2dc9f23924706eca62595f438224f29501bd3135
jfalxa <jfalxa.sta@gmail.com>
2020-07-21 13:44:18 +0200

    client: style workout list

--- README.md
+++ README.md
@@ -7,8 +7,10 @@ The front-end will be accessible at http://localhost:3000, the backend at http:/
 ## Front-end stack
 
 - `create-react-app` for the build config and base code
+- `styled-components` for styling directly in JS
 
 ## Back-end stack
 
 - `express` for the http server framework
 - `nodemon` for server run state management
+- `lowdb` for data management
--- client/src/components/workout-list.js
+++ client/src/components/workout-list.js
@@ -1,11 +1,86 @@
 import React from "react";
+import styled from "styled-components";
+import format from "date-fns/format";
+
+const categories = {
+  c1: "Daniel",
+  c2: "Rosario",
+  c3: "Serrano",
+  c4: "Brady",
+  c5: "Berger",
+  c6: "King",
+  c7: "Haley"
+};
+
+const colors = {
+  c1: "gold",
+  c2: "silver",
+  c3: "orange",
+  c4: "lime",
+  c5: "lightskyblue",
+  c6: "hotpink",
+  c7: "dodgerblue"
+};
+
+const List = styled.ul`
+  padding: 0;
+  margin: 0;
+  list-style: none;
+`;
+
+const Link = styled.a`
+  display: flex;
+  align-items: center;
+  padding: 12px;
+  color: initial;
+  text-decoration: none;
+
+  :hover {
+    background-color: #ddd;
+  }
+`;
+
+const Category = styled.span`
+  width: 48px;
+  background: ${(props) => colors[props.category]};
+  border-radius: 3px;
+  font-size: 10px;
+  text-align: center;
+  padding: 4px;
+`;
+
+const Name = styled.span`
+  font-weight: bold;
+  text-transform: capitalize;
+  margin-left: 8px;
+`;
+
+const StartDate = styled.span`
+  font-size: 14px;
+  margin-left: 8px;
+  color: #666;
+`;
+
+const WorkoutItem = ({ workout }) => (
+  <li>
+    <Link href={`/workouts/${workout.id}`}>
+      <Category category={workout.category}>
+        {categories[workout.category]}
+      </Category>
+      <Name>{workout.name}</Name>
+      <StartDate>
+        starting {format(new Date(workout.startDate), "PPP")}
+      </StartDate>
+    </Link>
+  </li>
+);
 
 const WorkoutList = ({ workouts }) => (
-  <ul>
+  <List>
     {workouts.map((workout) => (
-      <li key={workout.id}>{workout.name}</li>
+      <WorkoutItem key={workout.id} workout={workout} />
     ))}
-  </ul>
+  </List>
 );
 
 export default WorkoutList;
