bbd42d76f8d31e91ad8ced229dc92500d764b897
jfalxa <jfalxa.sta@gmail.com>
2020-07-21 11:58:49 +0200

    generate fake database in the server

---
+++ server/scripts/fake-db.js
@@ -0,0 +1,25 @@
+const pt = require("path");
+const fs = require("fs/promises");
+const faker = require("faker");
+
+const db = { workouts: [] };
+const categories = ["c1", "c2", "c3", "c4", "c5", "c6", "c7"];
+
+for (let i = 0; i < 1000; i++) {
+  const workout = {
+    id: i,
+    name: faker.random.words(2),
+    description: faker.lorem.sentence(6),
+    startDate: faker.date.between("2020-01-01", "2020-12-31"),
+    category: faker.random.arrayElement(categories)
+  };
+
+  db.workouts.push(workout);
+}
+
+const path = pt.resolve("db.json");
+const json = JSON.stringify(db, null, "  ");
+
+fs.writeFile(path, json, "utf-8")
+  .then(() => console.log("DB generated!"))
+  .catch((err) => console.log("An error happened during DB generation", err));
