03f319cda6086677c75b936f3539e698c5602cef
jfalxa <jfalxa.sta@gmail.com>
2020-07-22 13:15:21 +0200

    client: manage errors and loading in workout list

--- client/src/routes/workout-list.js
+++ client/src/routes/workout-list.js
@@ -1,14 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import styled from "styled-components";
 
 import { getAllWorkouts } from "../api";
+import usePromise from "../hooks/use-promise";
 import Pagination from "../components/pagination";
 import WorkoutList from "../components/workout-list";
 import FilterDate from "../components/filter-date";
 import FilterCategories from "../components/filter-categories";
 
 const LIMIT = 20;
+
+async function fetchWorkouts(page, startDate, categories) {
+  const options = { page, limit: LIMIT };
+
+  startDate.length > 0 && (options.startDate = startDate);
+  categories.length > 0 && (options.categories = categories);
+
+  return getAllWorkouts(options);
+}
 
 const FilterBox = styled.div`
   background-color: #eee;
@@ -21,9 +31,6 @@ const WorkoutListRoute = () => {
   const params = useParams();
   const history = useHistory();
 
-  const [total, setTotal] = useState(0);
-  const [workouts, setWorkouts] = useState([]);
-
   // filters
   const [startDate, setStartDate] = useState("");
   const [categories, setCategories] = useState([]);
@@ -31,27 +38,19 @@ const WorkoutListRoute = () => {
   // parse page number from url param
   const page = parseInt(params.page, 10) - 1;
 
+  // fetch the wanted list of workouts when a new page is loaded or filters change
+  const workouts = usePromise(fetchWorkouts, [page, startDate, categories]);
+  const workoutList = workouts.value?.data;
+  const total = workouts.value?.meta?.total;
+
   function goToPage(num) {
     history.push(`/list/${num + 1}`);
   }
 
-  // go back to page 1 when filters are changed
-  useEffect(() => {
+  // go back to first page if we're pointing too far
+  if (page < 0 || page >= total) {
     goToPage(0);
-  }, [startDate, categories]);
-
-  // fetch the wanted list of workouts when a new page is loaded
-  useEffect(() => {
-    const options = { page, limit: LIMIT };
-
-    startDate.length > 0 && (options.startDate = startDate);
-    categories.length > 0 && (options.categories = categories);
-
-    getAllWorkouts(options).then((res) => {
-      setTotal(res.meta.total);
-      setWorkouts(res.data);
-    });
-  }, [page, startDate, categories]);
+  }
 
   // prepare pagination to put it in 2 places if neeeded
   const pagination = total > 1 && (
@@ -66,7 +65,18 @@ const WorkoutListRoute = () => {
       </FilterBox>
 
       {pagination}
-      <WorkoutList workouts={workouts} />
+
+      {workouts.loading && <span>Loading...</span>}
+
+      {workouts.error && (
+        <span>
+          An error happened while loading the workout list:
+          {workouts.error.message}
+        </span>
+      )}
+
+      {workouts.value && <WorkoutList workouts={workoutList} />}
+
       {pagination}
     </React.Fragment>
   );
