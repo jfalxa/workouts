c68be52e4430aa5d4f2b15a07ae1381211e0f5f5
jfalxa <jfalxa.sta@gmail.com>
2020-07-22 11:58:35 +0200

    client: handle errors in api helper

--- client/src/api.js
+++ client/src/api.js
@@ -2,9 +2,23 @@ import { stringify } from "query-string";
 
 const API_ROOT = `http://${window.location.hostname}:3001`;
 
-function api(endpoint, options) {
+async function api(endpoint, options) {
   const queryString = options ? "?" + stringify(options) : "";
-  return fetch(API_ROOT + endpoint + queryString).then((res) => res.json());
+
+  const res = await fetch(API_ROOT + endpoint + queryString);
+  const contentType = res.headers.get("content-type");
+
+  if (!res.ok && !contentType.includes("application/json")) {
+    throw new Error(`Server error ${res.status}`);
+  }
+
+  const json = await res.json();
+
+  if (json.error) {
+    throw new Error(`API error: ${json.error}`);
+  }
+
+  return json;
 }
 
 export function getAllWorkouts(options) {
