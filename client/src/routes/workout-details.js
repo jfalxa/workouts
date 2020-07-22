871792bc6c108607136f02eda99a811bd8323c93
jfalxa <jfalxa.sta@gmail.com>
2020-07-22 13:14:59 +0200

    client: adapt workout details to new usePromise

--- client/src/routes/workout-details.js
+++ client/src/routes/workout-details.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 
 import { getWorkout } from "../api";
@@ -7,26 +7,25 @@ import WorkoutDetails from "../components/workout-details";
 
 const WorkoutDetailsRoute = () => {
   const { id } = useParams();
-  const workout = usePromise(getWorkout, null);
 
   // fetch the single workout matching the current route
-  useEffect(() => {
-    workout.run(id);
-  }, [id]);
+  const workout = usePromise(getWorkout, [id]);
+  const workoutDetails = workout.value?.data;
 
   if (workout.loading) {
     return <span>Loading...</span>;
   }
 
-  if (workout.error)
+  if (workout.error) {
     return (
       <span>
         An error happened while loading the workout details:
         {workout.error.message}
       </span>
     );
+  }
 
-  return <WorkoutDetails workout={workout.value?.data} />;
+  return <WorkoutDetails workout={workoutDetails} />;
 };
 
 export default WorkoutDetailsRoute;
