03f42ac280fe6756ff41b29ae44f05a2c324cafb
jfalxa <jfalxa.sta@gmail.com>
2020-07-22 13:56:43 +0200

    client: make pagination arrows regular text without link

--- client/src/components/pagination.js
+++ client/src/components/pagination.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
 
 const Nav = styled.nav`
   display: flex;
@@ -8,8 +7,9 @@ const Nav = styled.nav`
   margin: 16px;
 `;
 
-const Arrow = styled(Link)`
+const Arrow = styled.span`
   margin: 0 8px;
+  cursor: pointer;
   color: initial;
   text-decoration: none;
   color: ${(props) => (props.disabled ? "gray" : "black")};
@@ -24,11 +24,11 @@ const Menu = styled.select`
 
 const Pagination = ({ page, total, onChange }) => {
   const pages = [...Array(total).keys()]; // create an array with 0,1,2,3,...,total-1
-  const clamp = (value) => Math.max(1, Math.min(value, total)); // keep the value between 0 and total
+  const clamp = (value) => Math.max(0, Math.min(value, total - 1)); // keep the value between 0 and total
 
   return (
     <Nav>
-      <Arrow disabled={page === 0} to={`/list/${clamp(page)}`}>
+      <Arrow disabled={page === 0} onClick={() => onChange(clamp(page - 1))}>
         ◀
       </Arrow>
 
@@ -41,7 +41,10 @@ const Pagination = ({ page, total, onChange }) => {
         ))}
       </Menu>
 
-      <Arrow disabled={page === total - 1} to={`/list/${clamp(page + 2)}`}>
+      <Arrow
+        disabled={page === total - 1}
+        onClick={() => onChange(clamp(page + 1))}
+      >
         ▶
       </Arrow>
     </Nav>
