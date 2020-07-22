3964154d3a4ef48c2c3035f34fe728ee7d2db724
jfalxa <jfalxa.sta@gmail.com>
2020-07-22 14:14:34 +0200

    client: put month list generation inside filter-date

--- client/src/components/filter-date.js
+++ client/src/components/filter-date.js
@@ -1,9 +1,17 @@
-import { format } from "date-fns";
 import React from "react";
 import styled from "styled-components";
+import format from "date-fns/format";
+import addMonths from "date-fns/addMonths";
 
-import months from "../constants/months";
 import { Label, Clear } from "./system";
+
+const now = new Date().setDate(1);
+const months = [now];
+
+// create a list of the 1st days of the next 12 months
+for (let i = 1; i < 12; i++) {
+  months.push(addMonths(months[i - 1], 1));
+}
 
 const DateBox = styled.div`
   display: flex;
