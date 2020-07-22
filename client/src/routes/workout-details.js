5a12000804395a48b28028485b2b53f18e506884
jfalxa <jfalxa.sta@gmail.com>
2020-07-22 11:59:23 +0200

    client: handle loading and errors in workout details

--- client/src/routes/workout-details.js
+++ client/src/routes/workout-details.js
@@ -2,18 +2,31 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import { getWorkout } from "../api";
+import usePromise from "../hooks/use-promise";
 import WorkoutDetails from "../components/workout-details";
 
 const WorkoutDetailsRoute = () => {
   const { id } = useParams();
-  const [workout, setWorkout] = useState(null);
+  const workout = usePromise(getWorkout, null);
 
   // fetch the single workout matching the current route
   useEffect(() => {
-    getWorkout(id).then((res) => setWorkout(res.data));
+    workout.run(id);
   }, [id]);
 
-  return <WorkoutDetails workout={workout} />;
+  if (workout.loading) {
+    return <span>Loading...</span>;
+  }
+
+  if (workout.error)
+    return (
+      <span>
+        An error happened while loading the workout details:
+        {workout.error.message}
+      </span>
+    );
+
+  return <WorkoutDetails workout={workout.value?.data} />;
 };
 
 export default WorkoutDetailsRoute;
